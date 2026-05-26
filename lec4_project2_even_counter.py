a6c6399831c1d270c6caaa1cb8a44dffe8baebc2
Shivanshu Maurya <blacksrdx@gmail.com>
2026-05-26 09:05:23 +0530

    Add Lecture 4 Project 2: Even Number Counter
    Implemented a Python script that counts the total number of even numbers within a specific range using a for-loop and the modulo operator.

---
+++ lec4_project2_even_counter.py
@@ -0,0 +1,9 @@
+#CHECKING NUM OF EVEN NUM IN RANGE 
+
+even_num = 0
+
+for i in range (10): # i =
+    if (i%2 == 0 ): # remender of i/2 is zero 
+     even_num+=1
+ # after the loop is finish the below code run    
+print(even_num)    
