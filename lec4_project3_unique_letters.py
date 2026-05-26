1f7751555bf433ad99743e177525d3ea9823ddf5
Shivanshu Maurya <blacksrdx@gmail.com>
2026-05-26 09:35:33 +0530

    Update lec4_project3_unique_letters.py

--- lec4_project3_unique_letters.py
+++ lec4_project3_unique_letters.py
@@ -1,4 +1,35 @@
-s= "demo loops - fruit loops"
-for index in range(len(s)):
-    if s[index]=='i' or s[index]=='u' :
-        print('There is an i or u')
+#WRONG CODE❌
+
+# s = 'abca'
+# count = 0
+# seen = ''
+# for i in s:
+#     if i !=seen:
+#         count+=1
+#         seen = i
+   
+# print(count)        
+# print(seen)
+
+
+
+
+# 💡 LESSON LEARNED:
+#wrong code we use != for numerical value or like a!=b etc 
+#for boolen we can use the opposite of in (not in )
+#here we deal with string not numbers 
+
+
+#  CORRECT CODE ✅:
+
+s = 'abca'
+seen = ''#string
+count = 0
+for char in s: # to iterates the value/ letter of s seq wise
+    if char not in seen :
+        seen = seen + char # seen = seen + char
+        count+= 1
+        
+        
+print(count)
+print(seen)
