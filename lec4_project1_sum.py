7c722b5c09bd28b87fbffbe1e3bfdce58c7e60bc
Shivanshu Maurya <blacksrdx@gmail.com>
2026-05-26 08:58:55 +0530

    Add Lecture 4 Project 1: Sum Calculator
    Implemented the first project from Lecture 4, which calculates the sum of numbers in a given range using a for-loop and demonstrates a conditional break statement.

---
+++ lec4_project1_sum.py
@@ -0,0 +1,13 @@
+mysum = 0 
+for i in range(5,11,2): #here i = 5,7,9
+    mysum+=i
+     
+    if mysum == 5 :
+        break
+        mysum+=1
+        
+   
+    
+print(mysum)     #output = 5    
+    
+    
